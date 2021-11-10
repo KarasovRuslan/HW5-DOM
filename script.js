c2346be06612f4c1f7d109474a1494429f3a62ee
KarasovRuslan <karasov.ruslan@icloud.com>
2021-11-10 14:33:02 +0200

    HW5 commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("log_button").addEventListener("click", function logFun() {
+    document.getElementById("user_out").style.display = "flex";
+    document.getElementById("log_out").innerHTML = 'Login: ' + document.getElementById("user_name").value;
+    document.getElementById("pass_out").innerHTML = 'Password: ' + document.getElementById("user_pass").value;
+    localStorage.setItem('Login', (document.getElementById("user_name").value))
+    localStorage.setItem('Password', (document.getElementById("user_pass").value))
+
+    // localStorage.setItem('Login', JSON.stringify({key: 123}))
+});
