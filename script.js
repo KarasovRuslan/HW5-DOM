c059a9f300d79c09e16cfd649105529c03806ada
KarasovRuslan <karasov.ruslan@icloud.com>
2021-11-10 18:07:42 +0200

    HW5 commit

--- script.js
+++ script.js
@@ -4,6 +4,4 @@ document.getElementById("log_button").addEventListener("click", () => {
     document.getElementById("pass_out").innerHTML = 'Password: ' + document.getElementById("user_pass").value;
     localStorage.setItem('Login', (document.getElementById("user_name").value))
     localStorage.setItem('Password', (document.getElementById("user_pass").value))
-
-    // localStorage.setItem('Login', JSON.stringify({key: 123}))
 });
