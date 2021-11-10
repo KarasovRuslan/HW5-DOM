4389b13742af737832817c976c619e1ed02e999b
KarasovRuslan <karasov.ruslan@icloud.com>
2021-11-10 14:42:47 +0200

    HW5 commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.getElementById("log_button").addEventListener("click", function logFun() {
+document.getElementById("log_button").addEventListener("click", () => {
     document.getElementById("user_out").style.display = "flex";
     document.getElementById("log_out").innerHTML = 'Login: ' + document.getElementById("user_name").value;
     document.getElementById("pass_out").innerHTML = 'Password: ' + document.getElementById("user_pass").value;
